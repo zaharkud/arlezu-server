e2c16dbbaca6120a24536552e49cdc626fc826ce
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-09-23 15:40:44 +0300

    Added github actions

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 6000;
 
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
